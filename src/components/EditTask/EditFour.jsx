b44578212eab0004cded445e86b7c0038c23ac2a
ashokSoftSuave <81176222+ashokSoftSuave@users.noreply.github.com>
2025-01-08 17:32:26 +0000

    initial editors

---
+++ src/components/EditTask/EditFour.jsx
@@ -0,0 +1,6 @@
+const EditThree = () => {
+    return <div>Edit4</div>;
+  };
+  
+  export default EditFour;
+  
---
+++ src/components/EditTask/EditTwo.jsx
@@ -0,0 +1,6 @@
+const EditTwo = () => {
+    return <div>Edit2</div>;
+  };
+  
+  export default EditTwo;
+  
