419f04eacee90c4cc58a0c4280c508ec8c129864
ashokSoftSuave <81176222+ashokSoftSuave@users.noreply.github.com>
2025-01-08 17:29:53 +0000

    get state from useLocation

--- src/components/EditTask/EditTask.jsx
+++ src/components/EditTask/EditTask.jsx
@@ -1,5 +1,8 @@
-const EditTask = () =>{
+import { useLocation } from "react-router-dom";
 
+const EditTask = () =>{
+    const location = useLocation();
+    const [formData, setFormData] = useState(location.state);
 
 
     return (<div>edit task</div>)
